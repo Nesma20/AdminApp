04d1e98a528e9cf909628271d1a07849c50ed7b3
Nesma Tharwat <Nesma@learniosfreecoms-mac.local>
2019-05-31 11:08:50 +0200

    Initial Commit

---
+++ AdminApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdminApp
+//
+//  Created by Nesma Tharwat on 5/31/19.
+//  Copyright © 2019 Jets39. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
